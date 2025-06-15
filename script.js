f1c3182178f24449e65c1a71d20913ce68e97612
Bhagwati579 <bhagwatilalpushkarna123@gmail.com>
2025-06-15 13:34:53 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const taskBtn = document.getElementById("start-task");
-  if (taskBtn) {
-    taskBtn.addEventListener("click", function () {
-      alert("Coming soon... Get ready to earn ₹2000!");
-    });
-  }
-});
