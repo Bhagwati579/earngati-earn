b78ec63db697ad3dab0cbbb08d0886dbf28c6d79
Bhagwati579 <bhagwatilalpushkarna123@gmail.com>
2025-06-19 09:15:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+
+document.getElementById("earnBtn").onclick = function() {
+    document.getElementById("taskBox").innerHTML = "<h2>Task Assigned</h2><p>Your task ID is #TG-9201. Complete it to get ₹2000.</p>";
+};
