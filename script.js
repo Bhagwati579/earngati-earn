10cf5075d75b20f3f4022571ea2ed8dc38c8784a
Bhagwati579 <bhagwatilalpushkarna123@gmail.com>
2025-06-22 19:30:37 +0530

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-
-document.getElementById("earnBtn").onclick = function() {
-    document.getElementById("taskBox").innerHTML = "<h2>Task Assigned</h2><p>Your task ID is #TG-9201. Complete it to get ₹2000.</p>";
-};
