b8d2dc9d01bf5a46573c51d54c3004be312faa7d
Bhagwati579 <bhagwatilalpushkarna123@gmail.com>
2025-06-29 18:57:55 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-function startTask() {
-  document.getElementById('status').innerText = "Task started. Please wait...";
-  setTimeout(() => {
-    document.getElementById('status').innerText = "✅ Task complete! ₹2000 added to your UPI.";
-  }, 5000);
-}
